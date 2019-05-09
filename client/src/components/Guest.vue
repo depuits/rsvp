f56ceb8387b65f3ad273ccbb76da7e88a187379b
Colman Joeri <colmanjoeri@msn.com>
2019-05-09 08:19:53 +0200

    fixed warnings

--- client/src/components/Guest.vue
+++ client/src/components/Guest.vue
@@ -1,13 +1,13 @@
 <template>
 	<li class="columns">
 		<div class="column">
-			<b-field horizontal label="Code"><b-input v-model="guest.info.code" readonly /></b-field>
+			<b-field horizontal label="Code"><b-input v-model="guest.info.code" readonly/></b-field>
 
-			<b-field horizontal grouped :label="i ? '' : 'names'"  v-for="(n, i) in guest.info.names" :key="i">
+			<b-field v-for="(n, i) in guest.info.names" :key="i" :label="i ? '' : 'names'" grouped horizontal>
 				<div class="field has-addons is-expanded">
 					<b-input v-model="guest.info.names[i]" @change="change" />
 					<div class="control">
-						<b-button @click="removeName(i)" type="is-danger"><b-icon icon="account-remove" /></b-button>
+						<b-button type="is-danger" @click="removeName(i)"><b-icon icon="account-remove"/></b-button>
 					</div>
 				</div>
 			</b-field>
@@ -53,11 +53,11 @@ export default {
 	methods: {
 		addName: function() {
 			this.guest.info.names.push('');
-			change();
+			this.change();
 		},
 		removeName: function(i) {
 			this.guest.info.names.splice(i, 1);
-			change();
+			this.change();
 		},
 		remove: function() {
 			Api()
