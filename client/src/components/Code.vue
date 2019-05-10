84601be83a196bdabdbf7c8bc17027bce6fef18e
depuits <colmanjoeri@gmail.com>
2019-05-10 19:17:30 +0200

    fixed submit on code component

--- client/src/components/Code.vue
+++ client/src/components/Code.vue
@@ -2,28 +2,30 @@
 	<div id="code">
 		<h1>{{ $t('login.prompt') }}</h1>
 
-		<b-field position="is-centered" :type="response ? 'is-danger' : 'is-primary'">
-			<b-input
-				v-model="code"
-				type="password"
-				:placeholder="$t('login.code')"
-				:disabled="proccesing"
-				password-reveal
-				maxlength="8"
-				size="is-large"
-			/>
+		<form @submit.prevent="login">
+			<b-field position="is-centered" :type="response ? 'is-danger' : 'is-primary'">
+				<b-input
+					v-model="code"
+					type="password"
+					:placeholder="$t('login.code')"
+					:disabled="proccesing"
+					password-reveal
+					maxlength="8"
+					size="is-large"
+				/>
 
-			<div class="control">
-				<b-button type="is-primary" :disabled="proccesing" size="is-large" @click="login()">
-					{{ $t('login.btn.submit') }}
-					<div v-if="proccesing" class="overlay tint">
-						<img class="lds-heart backColor" src="../assets/images/heart.png" />
-					</div>
-				</b-button>
-			</div>
-		</b-field>
+				<div class="control">
+					<b-button type="is-primary" :disabled="proccesing" size="is-large" native-type="submit">
+						{{ $t('login.btn.submit') }}
+						<div v-if="proccesing" class="overlay tint">
+							<img class="lds-heart backColor" src="../assets/images/heart.png" />
+						</div>
+					</b-button>
+				</div>
+			</b-field>
 
-		<p v-if="!proccesing" class="has-text-danger">{{ $t(response) }}</p>
+			<p v-if="!proccesing" class="has-text-danger">{{ $t(response) }}</p>
+		</form>
 	</div>
 </template>
 
