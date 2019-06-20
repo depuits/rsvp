15fdc0c83818f8df7ce8b230db6ae0f547f43023
Colman Joeri <colmanjoeri@msn.com>
2019-06-20 09:29:22 +0200

    changed logging added rate limit on requests

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const history = require('connect-history-api-fallback');
+const rateLimit = require("express-rate-limit");
 
 const config = require('config');
 const port = config.get('port');
@@ -13,12 +14,24 @@ const mongo = require('./lib/mongoUtil');
 const app = express();
 
 mongo.connect().then(() => {
-	app.use(morgan('dev'));
+	// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+	// see https://expressjs.com/en/guide/behind-proxies.html
+	app.set('trust proxy', 1);
+
+	// limit requests to 1.8 seconds per request for 15 minutes
+	const apiLimiter = rateLimit({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 500,
+	});
+
+	app.use("/api/", apiLimiter);
+
+	app.use(morgan(':remote-addr - [:date[iso]] ":method :url" :status - :response-time ms - :res[content-length]'));
 	app.use(bodyParser.json());
 	app.use(cors());
 
 	app.use('/api', require('./routes')); // api endpoints
-	
+
 	app.use(express.static('public')); // static resources
 
 	const staticFileMiddleware = express.static('client'); // client resources
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const rateLimit = require("express-rate-limit");
+
+const responseLimiter = rateLimit({
+    windowMs: 30 * 60 * 1000, // half hour window
+    max: 20, // start blocking after 20 requests
+});
 
 // mount our routes onto the API router
 router.use('/shedule', require('./shedule'));
 router.use('/history', require('./history'));
-router.use('/response', require('./response'));
+router.use('/response', responseLimiter, require('./response'));
 
 module.exports = router;
